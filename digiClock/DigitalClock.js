48ea96d98722d9fa3386c26236ebdb8f230267c6
Maceo Lewis <maceo.code@gmail.com>
2019-07-24 14:41:49 -0400

    Formatting for time and date complete

--- digiClock/DigitalClock.js
+++ digiClock/DigitalClock.js
@@ -1,12 +1,32 @@
 function showTime() {
-    const date = new Date();
+    options = {weekday: 'short', year: 'numeric', month: 'short', day: '2-digit'}
+    date = new Date();
     hr = date.getHours();
     min = date.getMinutes();
     sec = date.getSeconds();
+    period = "am";
+
+    if(hr == 0){
+        hr = 12;
+    }
+
+    if(hr >= 12){
+        hr = hr - 12;
+        period = "pm";
+    }
+
     
-    var time = hr + ":" + min + ":" + sec;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    var time = hr + ":" + min + ":" + sec + " " + period; 
+    document.getElementById("displayTime").innerHTML = time;
+    document.getElementById("displayDate").innerHTML = date.toLocaleDateString("en-US", options);
+
+
+
+
     
-    document.getElementById("displayTime").textContent = time;
 }
 
 showTime();
