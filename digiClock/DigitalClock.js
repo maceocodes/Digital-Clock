96e29c2ced7993a1bcfab268e7ccc0b396ebc783
Maceo Lewis <maceo.code@gmail.com>
2019-08-13 15:48:06 -0400

    still working on user input

--- digiClock/DigitalClock.js
+++ digiClock/DigitalClock.js
@@ -1,42 +1,51 @@
 
+function one(){
 
-function showTime() {
-    options = {weekday: 'short', year: 'numeric', month: 'short', day: '2-digit'}
-    var date = new Date();
-    var hr = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    var period = "am";
 
-    if(hr == 0){
-        hr = 12;
-    }
-
-    if(hr > 12){
-        hr = hr - 12;
-        period = "pm";
-    }
-
-    min = (min < 10) ? "0" + min : min;
-    sec = (sec < 10) ? "0" + sec : sec;
+    function showTime() {
+        var options = {weekday: 'short', year: 'numeric', month: 'short', day: '2-digit'}
+        var date = new Date();
+        var hr = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();
+        var period = "am";
     
+        if(hr == 0){
+            hr = 12;
+        }
+    
+        if(hr > 12){
+            hr = hr - 12;
+            period = "pm";
+        }
+    
+        var min = (min < 10) ? "0" + min : min;
+        var sec = (sec < 10) ? "0" + sec : sec;
+        
+    
+        var time = hr + ":" + min + ":" + sec + " " + period; 
+        
+        document.getElementById("displayTime").innerHTML = time;
+        document.getElementById("displayDate").innerHTML = date.toLocaleDateString("en-US", options);
+    
+        startTime = setTimeout(showTime, 1000);
+    
+    }
+    
+    showTime(); 
 
-    var time = hr + ":" + min + ":" + sec + " " + period; 
-    document.getElementById("displayTime").innerHTML = time;
-    document.getElementById("displayDate").innerHTML = date.toLocaleDateString("en-US", options);
-
-    var startTime = setTimeout(showTime, 1000);
-
-}
-
-showTime();
+} 
+one();
 
 
-//toggle input, clock visibility, and cancel button display
+
+//toggle input, clock and cancel button, set/save button display.  
 var set = document.getElementById("setBttn");
 var timeVis = document.getElementById("displayTime");
 var cancelVis = document.getElementById("cancelBttn");
 var inputVis = document.getElementById("setInput");
+var amVis = document.getElementById("am");
+var pmVis = document.getElementById("pm");
 
 
 var eToHide = document.getElementsByClassName("savOrCncl");
@@ -54,40 +63,30 @@ var setOrSave = (function() {
   })();
 
 function enterTime() {
-    //toggle time visibility 
-    if (timeVis.style.display !== 'none') {
-        timeVis.style.display = 'none';
-    } else {
-        timeVis.style.display = 'block';
-    }          
-
-    //toggle input visibility
-    if (inputVis.style.display !== 'none') {
-        inputVis.style.display = 'none';
-    } else {
-        inputVis.style.display = 'block';
-    } 
-
-    //toggle cancelBttn visibility
-    if (cancelVis.style.display !== 'none') {
-        cancelVis.style.display = 'none';
-    } else {
-        cancelVis.style.display = 'block';
-    } 
-
-    //change text for setBttn upon clicking
-    if (set.innerText !== "set"){
-        set.innerText = "set";
-    } else {
-        set.innerText = 'save';
-    }
+    //initial click to enter time
+    timeVis.style.display = 'none';
+    inputVis.style.display = 'block';
+    cancelVis.style.display = 'block';
+    amVis.style.display = 'block';
+    pmVis.style.display = 'block';
+    set.innerText = 'save';
 };
 
 function saveTime() {
-    alert("do something else");
+    //second click to save entered time.
+    timeVis.style.display = 'block';
+    inputVis.style.display = 'none';
+    cancelVis.style.display = 'none';
+    amVis.style.display = 'none';
+    pmVis.style.display = 'none';
+    set.style.display = 'block';
+    set.innerText = 'set';
 };
 
-    //user input
+
+
+
+    //verify user input
 // function isNum(event){
 //     var e = event || window.event
 //     var key = e.keyCode || e.which;
@@ -101,6 +100,7 @@ function saveTime() {
 // } 
 
 
+//compare input to time and do a thing if it matches
 
 
 
